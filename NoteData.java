ba1d178dd65d895cc764a51b0388df570c9260a4
Bhaskar485 <56877669+Bhaskar485@users.noreply.github.com>
2019-11-10 17:43:31 +0530

    Add files via upload

---
+++ NoteData.java
@@ -0,0 +1,33 @@
+package com.example.hellonotepad;
+
+public class NoteData {
+
+    String title;
+    String description;
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
